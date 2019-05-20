b9a4e8f46055feb0d79ff1d601c87221df5b4d59
Sveinung Fjær <sveinung.fjaer@webstep.no>
2019-05-20 14:51:14 +0200

    Removed trailing /

--- KS.Fiks.IO.Client/Configuration/DokumentlagerConfiguration.cs
+++ KS.Fiks.IO.Client/Configuration/DokumentlagerConfiguration.cs
@@ -2,7 +2,7 @@ namespace KS.Fiks.IO.Client.Configuration
 {
     public class DokumentlagerConfiguration : ApiConfiguration
     {
-        private const string DefaultDownloadPath = "/dokumentlager/nedlasting/";
+        private const string DefaultDownloadPath = "/dokumentlager/nedlasting";
 
         public DokumentlagerConfiguration(string scheme = null, string host = null, int? port = null, string downloadPath = null)
         : base(scheme, host, port)
