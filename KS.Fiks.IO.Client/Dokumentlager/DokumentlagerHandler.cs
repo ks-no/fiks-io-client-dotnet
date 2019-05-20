5a9a6f79389e24123504803a58a329ed75e8dec8
Sveinung Fjær <sveinung.fjaer@webstep.no>
2019-05-20 14:45:50 +0200

    Use maskinporten to authenticate for dokumentlager

--- KS.Fiks.IO.Client/Dokumentlager/DokumentlagerHandler.cs
+++ KS.Fiks.IO.Client/Dokumentlager/DokumentlagerHandler.cs
@@ -4,33 +4,51 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using KS.Fiks.IO.Client.Configuration;
 using KS.Fiks.IO.Client.Exceptions;
+using KS.Fiks.IO.Send.Client.Authentication;
+using Ks.Fiks.Maskinporten.Client;
 
 namespace KS.Fiks.IO.Client.Dokumentlager
 {
     internal class DokumentlagerHandler : IDokumentlagerHandler
     {
-        private readonly DokumentlagerConfiguration _configuration;
+        private readonly DokumentlagerConfiguration _dokumentlagerConfiguration;
 
         private readonly HttpClient _httpClient;
 
-        public DokumentlagerHandler(DokumentlagerConfiguration configuration, HttpClient httpClient = null)
+        private readonly IAuthenticationStrategy _authenticationStrategy;
+
+        public DokumentlagerHandler(
+            DokumentlagerConfiguration dokumentlagerConfiguration,
+            IntegrationConfiguration integrationConfiguration,
+            IMaskinportenClient maskinportenClient,
+            IAuthenticationStrategy authenticationStrategy = null,
+            HttpClient httpClient = null)
         {
-            _configuration = configuration;
+            _dokumentlagerConfiguration = dokumentlagerConfiguration;
+            _authenticationStrategy = authenticationStrategy ??
+                                      new IntegrasjonAuthenticationStrategy(maskinportenClient, integrationConfiguration.IntegrationId, integrationConfiguration.IntegrationPassword);
             _httpClient = httpClient ?? new HttpClient();
         }
 
         public async Task<Stream> Download(Guid messageId)
         {
-            var response = await _httpClient.SendAsync(CreateRequestMessage(messageId)).ConfigureAwait(false);
+            var response = await _httpClient.SendAsync(await CreateRequestMessage(messageId).ConfigureAwait(false)).ConfigureAwait(false);
             ThrowIfContentIsEmpty(response, messageId);
             return await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
         }
 
-        private HttpRequestMessage CreateRequestMessage(Guid messageId)
+        private async Task<HttpRequestMessage> CreateRequestMessage(Guid messageId)
         {
-            var uri = new UriBuilder(_configuration.Scheme, _configuration.Host, _configuration.Port,$"{_configuration.DownloadPath}/{messageId}").Uri;
+            var uri = new UriBuilder(_dokumentlagerConfiguration.Scheme, _dokumentlagerConfiguration.Host, _dokumentlagerConfiguration.Port, $"{_dokumentlagerConfiguration.DownloadPath}/{messageId}").Uri;
 
-            return new HttpRequestMessage(HttpMethod.Get, uri);
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            foreach (var keyValuePair in await _authenticationStrategy
+                                               .GetAuthorizationHeaders().ConfigureAwait(false))
+            {
+                requestMessage.Headers.Add(keyValuePair.Key, keyValuePair.Value);
+            }
+
+            return requestMessage;
         }
 
         private void ThrowIfContentIsEmpty(HttpResponseMessage responseMessage, Guid messageId)
