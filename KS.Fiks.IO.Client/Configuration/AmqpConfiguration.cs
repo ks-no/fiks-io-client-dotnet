b8b1f06546f7ea5229188079e2bc67b28e9b40ed
Sveinung Fjær <sveinung.fjaer@webstep.no>
2019-05-27 09:17:27 +0200

    Add default SslOptions

--- KS.Fiks.IO.Client/Configuration/AmqpConfiguration.cs
+++ KS.Fiks.IO.Client/Configuration/AmqpConfiguration.cs
@@ -1,14 +1,22 @@
+using System.Net.Security;
 using RabbitMQ.Client;
 
 namespace KS.Fiks.IO.Client.Configuration
 {
     public class AmqpConfiguration
     {
+        private const string DefaultSslServerName = "minside.fiks.ks.no";
+
         public AmqpConfiguration(string host, int port = 5671, SslOption sslOption = null)
         {
             Host = host;
             Port = port;
-            SslOption = sslOption;
+            SslOption = sslOption ?? new SslOption
+            {
+                Enabled = true,
+                ServerName = DefaultSslServerName,
+                CertificateValidationCallback = (sender, certificate, chain, errors) => errors == SslPolicyErrors.None
+            };
         }
 
         public string Host { get; }
