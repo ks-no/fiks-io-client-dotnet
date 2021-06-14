bba6df1e612fc2922c5d8e11a7f7944b9d86c1b7
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-06-14 19:43:17 +0200

    Lagt til MeldingTypeV1.cs (#48)

---
+++ KS.Fiks.IO.Client/Models/MeldingTypeV1.cs
@@ -0,0 +1,8 @@
+namespace KS.Fiks.IO.Client.Models
+{
+    public static class MeldingTypeV1
+    {
+        public const string Ugyldigforespørsel = "no.ks.fiks.kvittering.ugyldigforespørsel.v1";
+        public const string Serverfeil = "no.ks.fiks.kvittering.serverfeil.v1";
+    }
+}
