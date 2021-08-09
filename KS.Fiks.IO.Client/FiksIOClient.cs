74595dbb5a6794e3015814cf942483251159c9a2
Erling Wisløff <70644618+ErHaWi@users.noreply.github.com>
2021-08-09 14:49:16 +0200

    HttpClient injection (#54)
    * payloads on receive
    
    * Honor disposable streams
    
    * extended constructor to allow injecting httpClient to avoid socket exhaustion
    
    * revert message extension for PR
    
    * gitignore and whitespace
    
    * Resure HttpClient for DokumentlagerHandler and CatalogHandler
    
    * Included maskinporten aswell
    
    * fix gitingore

--- KS.Fiks.IO.Client/FiksIOClient.cs
+++ KS.Fiks.IO.Client/FiksIOClient.cs
@@ -12,6 +12,7 @@ using KS.Fiks.IO.Client.Models;
 using KS.Fiks.IO.Client.Send;
 using Ks.Fiks.Maskinporten.Client;
 using RabbitMQ.Client.Events;
+using System.Net.Http;
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 
@@ -32,7 +33,7 @@ namespace KS.Fiks.IO.Client
         private readonly IPublicKeyProvider _publicKeyProvider;
 
         public FiksIOClient(FiksIOConfiguration configuration, IPublicKeyProvider publicKeyProvider = null)
-            : this(configuration, null, null, null, null, null, publicKeyProvider)
+            : this(configuration, null, null, null, null, null, null, publicKeyProvider)
         {
         }
 
@@ -43,16 +44,18 @@ namespace KS.Fiks.IO.Client
             ISendHandler sendHandler = null,
             IDokumentlagerHandler dokumentlagerHandler = null,
             IAmqpHandler amqpHandler = null,
+            HttpClient httpClient = null,
             IPublicKeyProvider publicKeyProvider = null)
         {
             KontoId = configuration.KontoConfiguration.KontoId;
 
-            maskinportenClient = maskinportenClient ?? new MaskinportenClient(configuration.MaskinportenConfiguration);
+            maskinportenClient = maskinportenClient ?? new MaskinportenClient(configuration.MaskinportenConfiguration, httpClient);
 
             _catalogHandler = catalogHandler ?? new CatalogHandler(
                                   configuration.KatalogConfiguration,
                                   configuration.IntegrasjonConfiguration,
-                                  maskinportenClient);
+                                  maskinportenClient,
+                                  httpClient);
 
             _publicKeyProvider = publicKeyProvider ?? new CatalogPublicKeyProvider(_catalogHandler);
 
@@ -64,10 +67,15 @@ namespace KS.Fiks.IO.Client
                                maskinportenClient,
                                configuration.FiksIOSenderConfiguration,
                                configuration.IntegrasjonConfiguration,
+                               httpClient,
                                asicEncrypter,
                                _publicKeyProvider);
 
-            _dokumentlagerHandler = dokumentlagerHandler ?? new DokumentlagerHandler(configuration.DokumentlagerConfiguration, configuration.IntegrasjonConfiguration, maskinportenClient);
+            _dokumentlagerHandler = dokumentlagerHandler ?? new DokumentlagerHandler(
+                configuration.DokumentlagerConfiguration,
+                configuration.IntegrasjonConfiguration,
+                maskinportenClient,
+                httpClient: httpClient);
 
             _amqpHandler = amqpHandler ?? new AmqpHandler(
                                maskinportenClient,
@@ -138,7 +146,7 @@ namespace KS.Fiks.IO.Client
 
         private async Task<SendtMelding> Send(MeldingRequest request, IPayload payload)
         {
-            return await Send(request, new List<IPayload> {payload}).ConfigureAwait(false);
+            return await Send(request, new List<IPayload> { payload }).ConfigureAwait(false);
         }
     }
 }
