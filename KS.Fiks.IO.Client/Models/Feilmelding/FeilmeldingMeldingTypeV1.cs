bc8214e2d61ade91f55ee033c121839c6de8e43c
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-12-08 13:55:05 +0100

    Ugyldigforespoersel schema manglet i csproj filen

--- KS.Fiks.IO.Client/Models/Feilmelding/FeilmeldingMeldingTypeV1.cs
+++ KS.Fiks.IO.Client/Models/Feilmelding/FeilmeldingMeldingTypeV1.cs
@@ -2,7 +2,7 @@ namespace KS.Fiks.IO.Client.Models.Feilmelding
 {
     public static class FeilmeldingMeldingTypeV1
     {
-        public const string Ugyldigforespørsel = "no.ks.fiks.kvittering.ugyldigforespørsel.v1";
+        public const string Ugyldigforespørsel = "no.ks.fiks.kvittering.ugyldigforespoersel.v1";
         public const string Serverfeil = "no.ks.fiks.kvittering.serverfeil.v1";
     }
 }
