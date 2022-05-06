2dbcb99f5237bf077dc81cfcb1e04c345a569640
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-05-06 15:34:37 +0200

    Flyttet feilmeldinger til eget repo ny nuget pakke

--- KS.Fiks.IO.Client/Models/Feilmelding/FeilmeldingMeldingTypeV1.cs
+++
@@ -1,9 +0,0 @@
-namespace KS.Fiks.IO.Client.Models.Feilmelding
-{
-    public static class FeilmeldingMeldingTypeV1
-    {
-        public const string Ugyldigforespørsel = "no.ks.fiks.kvittering.ugyldigforespoersel.v1";
-        public const string Serverfeil = "no.ks.fiks.kvittering.serverfeil.v1";
-        public const string Ikkefunnet = "no.ks.fiks.kvittering.ikkefunnet.v1";
-    }
-}
