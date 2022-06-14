21cbbbf95036f48479856fa2257bcca30e55139a
Jarle Børsheim <jarle.borsheim@sonat.no>
2022-06-14 10:18:21 +0200

    Channel blir ikke dipsoset av connection. La til dispose for channel

--- KS.Fiks.IO.Client/Amqp/AmqpHandler.cs
+++ KS.Fiks.IO.Client/Amqp/AmqpHandler.cs
@@ -77,6 +77,7 @@ namespace KS.Fiks.IO.Client.Amqp
         {
             if (disposing)
             {
+                _channel?.Dispose();
                 _connection?.Dispose();
             }
         }
