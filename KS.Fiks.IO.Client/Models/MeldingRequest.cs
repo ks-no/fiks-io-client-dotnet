30416947cf64d06e65fa55aaa8897020ae41f1aa
Jarle Børsheim <jarle.borsheim@sonat.no>
2025-04-10 20:12:41 +0200

    Kaster exception hvis man setter parameter og header med ulike verdier

--- KS.Fiks.IO.Client/Models/MeldingRequest.cs
+++ KS.Fiks.IO.Client/Models/MeldingRequest.cs
@@ -34,12 +34,34 @@ namespace KS.Fiks.IO.Client.Models
         {
             if (KlientMeldingId != null && KlientMeldingId != Guid.Empty)
             {
-                Headere.Add(HeaderKlientMeldingId, KlientMeldingId.ToString());
+                if (Headere != null && Headere.TryGetValue(HeaderKlientMeldingId, out var headerValue))
+                {
+                    if (!KlientMeldingId.ToString().Equals(headerValue))
+                    {
+                        throw new ArgumentException(
+                            $"Header dictionary contains a KlientMeldingId that doesn't match the KlientMeldingId as set in parameter. KlientMeldingId in header: {headerValue} - KlientMeldingId in parameter: {KlientMeldingId}");
+                    }
+                }
+                else
+                {
+                    Headere.Add(HeaderKlientMeldingId, KlientMeldingId.ToString());
+                }
             }
 
             if (!string.IsNullOrEmpty(KlientKorrelasjonsId))
             {
-                Headere.Add(HeaderKlientKorrelasjonsId, KlientKorrelasjonsId);
+                if (Headere != null && Headere.TryGetValue(HeaderKlientKorrelasjonsId, out var headerValue))
+                {
+                    if (!KlientKorrelasjonsId.Equals(headerValue))
+                    {
+                        throw new ArgumentException(
+                            $"Header dictionary contains a KlientKorrelasjonsId that doesn't match the KlientKorrelasjonsId as set in parameter. KlientKorrelasjonsId in header: {headerValue} - KlientKorrelasjonsId in parameter: {KlientKorrelasjonsId}");
+                    }
+                }
+                else
+                {
+                    Headere.Add(HeaderKlientKorrelasjonsId, KlientKorrelasjonsId);
+                }
             }
 
             return new MeldingSpesifikasjonApiModel(
