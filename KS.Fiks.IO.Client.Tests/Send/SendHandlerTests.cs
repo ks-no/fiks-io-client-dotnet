1faaf17466952a43170d6ddb3d67fd721f733d6f
Jarle Børsheim <jarle.borsheim@sonat.no>
2025-04-11 09:16:00 +0200

    Tester

--- KS.Fiks.IO.Client.Tests/Send/SendHandlerTests.cs
+++ KS.Fiks.IO.Client.Tests/Send/SendHandlerTests.cs
@@ -21,7 +21,7 @@ namespace KS.Fiks.IO.Client.Tests.Send
         }
 
         [Fact]
-        public async Task CallsFiksIOSenderSend()
+        public async Task Verify_Send_Message()
         {
             var sut = _fixture.CreateSut();
 
@@ -40,7 +40,7 @@ namespace KS.Fiks.IO.Client.Tests.Send
         }
 
         [Fact]
-        public async Task CallsSendWithExpectedMessageSpecificationApiModelWithKlientHeaders()
+        public async Task Verify_MessageSpecification_On_Send_With_ApiModel_With_KlientHeaders()
         {
             var sut = _fixture.CreateSut();
 
@@ -73,7 +73,83 @@ namespace KS.Fiks.IO.Client.Tests.Send
         }
 
         [Fact]
-        public async Task CallsSendWithExpectedMessageSpecificationApiModelAndNullKlientHeaders()
+        public async Task Verify_MessageSpecification_On_Send_With_ApiModel_With_To_Equal_KlientHeaders()
+        {
+            var sut = _fixture.CreateSut();
+            var klientKorrelasjonsId = Guid.NewGuid().ToString();
+            var klientMeldingId = Guid.NewGuid();
+
+            var request = new MeldingRequest(
+                avsenderKontoId: Guid.NewGuid(),
+                mottakerKontoId: Guid.NewGuid(),
+                klientMeldingId: klientMeldingId,
+                klientKorrelasjonsId: klientKorrelasjonsId,
+                meldingType: "Meldingsprotokoll",
+                ttl: TimeSpan.FromDays(2),
+                headere: new Dictionary<string, string>() {{MeldingBase.HeaderKlientKorrelasjonsId, klientKorrelasjonsId}, {MeldingBase.HeaderKlientMeldingId, klientMeldingId.ToString() }},
+                svarPaMelding: Guid.NewGuid());
+
+            var payload = new List<IPayload>();
+
+            await sut.Send(request, payload).ConfigureAwait(false);
+
+            _fixture.FiksIOSenderMock.Verify(
+                _ => _.Send(
+                    It.Is<MeldingSpesifikasjonApiModel>(
+                        model => model.Ttl == (long)request.Ttl.TotalMilliseconds &&
+                                 model.SvarPaMelding == request.SvarPaMelding &&
+                                 model.AvsenderKontoId == request.AvsenderKontoId &&
+                                 model.MottakerKontoId == request.MottakerKontoId &&
+                                 model.Headere[MeldingBase.HeaderKlientMeldingId] == request.KlientMeldingId.ToString() &&
+                                 model.Headere[MeldingBase.HeaderKlientKorrelasjonsId] == request.KlientKorrelasjonsId),
+                    It.IsAny<Stream>(),
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task Verify_Exception_Returned_When_Send_With_Two_Different_KlientKorrelasjonsId_Set()
+        {
+            var sut = _fixture.CreateSut();
+            var klientKorrelasjonsId1 = Guid.NewGuid().ToString();
+            var klientKorrelasjonsId2 = Guid.NewGuid().ToString();
+
+            var request = new MeldingRequest(
+                avsenderKontoId: Guid.NewGuid(),
+                mottakerKontoId: Guid.NewGuid(),
+                klientKorrelasjonsId: klientKorrelasjonsId1,
+                meldingType: "Meldingsprotokoll",
+                ttl: TimeSpan.FromDays(2),
+                headere: new Dictionary<string, string>() {{MeldingBase.HeaderKlientKorrelasjonsId, klientKorrelasjonsId2}},
+                svarPaMelding: Guid.NewGuid());
+
+            var payload = new List<IPayload>();
+            await Assert.ThrowsAsync<ArgumentException>(() => sut.Send(request, payload));
+        }
+
+        [Fact]
+        public async Task Verify_Exception_Returned_When_Send_With_Two_Different_KlientMeldingId_Set()
+        {
+            var sut = _fixture.CreateSut();
+            var klientMeldingId1 = Guid.NewGuid();
+            var klientMeldingId2 = Guid.NewGuid();
+
+            var request = new MeldingRequest(
+                avsenderKontoId: Guid.NewGuid(),
+                mottakerKontoId: Guid.NewGuid(),
+                klientMeldingId: klientMeldingId1,
+                meldingType: "Meldingsprotokoll",
+                ttl: TimeSpan.FromDays(2),
+                headere: new Dictionary<string, string>() {{MeldingBase.HeaderKlientMeldingId, klientMeldingId2.ToString()}},
+                svarPaMelding: Guid.NewGuid());
+
+            var payload = new List<IPayload>();
+            await Assert.ThrowsAsync<ArgumentException>(() => sut.Send(request, payload));
+        }
+
+
+        [Fact]
+        public async Task Verify_MessageSpecification_On_Send_With_ApiModel_With_Null_KlientHeaders()
         {
             var sut = _fixture.CreateSut();
 
@@ -104,7 +180,7 @@ namespace KS.Fiks.IO.Client.Tests.Send
         }
 
         [Fact]
-        public async Task CallsEncrypterWithExpectedPrivateKey()
+        public async Task Verify_Encrypter_With_Expected_PrivateKey()
         {
             var expectedPublicKey = _fixture.CreateTestCertificate();
             var sut = _fixture.WithPublicKey(expectedPublicKey).CreateSut();
@@ -117,7 +193,7 @@ namespace KS.Fiks.IO.Client.Tests.Send
         }
 
         [Fact]
-        public async Task GetsKeyFromCatalogHandler()
+        public async Task Verify_GetKey_From_CatalogHandler()
         {
             var sut = _fixture.CreateSut();
             var request = _fixture.DefaultRequest;
